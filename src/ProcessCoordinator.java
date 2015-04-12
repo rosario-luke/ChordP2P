bdf0fefa45021243e1061b8e32536cbe86bd9503
lucas rosario <lrosari2@illinois.edu>
2015-04-12 13:27:55 -0500

    Created Automatic Testing

--- src/ProcessCoordinator.java
+++ src/ProcessCoordinator.java
@@ -1,6 +1,7 @@
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -13,18 +14,22 @@ public class ProcessCoordinator extends Thread {
     private String oFile;
     private boolean outputToFile = false;
     private PrintWriter printWriter;
+
     public static void main(String[] args) {
         System.out.println("Started Coordinator Thread");
         if(args.length == 2){
-            new ProcessCoordinator().run(args[1]);
+            new ProcessCoordinator().run(args[1], false);
+        } else if(args.length == 3){
+            new ProcessCoordinator().run(args[1], true);
+        } else {
+            new ProcessCoordinator().run("", false);
         }
-        new ProcessCoordinator().run("");
 
     }
 
 
 
-    public void run(String outputFile){
+    public void run(String outputFile, boolean tf){
         if(!outputFile.equals("")){
             oFile = outputFile;
             outputToFile = true;
@@ -39,30 +44,79 @@ public class ProcessCoordinator extends Thread {
         zero = new ChordThread(0, null, printWriter);
         myThreads[0] = zero;
         zero.start();
-        print("Starting system");
-        Scanner input = new Scanner(System.in);
-        String curLine, command;
-        print("Listening for Commands");
-        while(!(curLine = input.nextLine()).equals("exit")){
-            print("Recieved command " + curLine);
-            command = curLine.split(" ")[0];
+        if(!tf) {
+            print("Starting system");
+            Scanner input = new Scanner(System.in);
+            String curLine, command;
+            print("Listening for Commands");
+            while (!(curLine = input.nextLine()).equals("exit")) {
+                print("Recieved command " + curLine);
+                command = curLine.split(" ")[0];
 
-            if(command.equals("join")){
-                int process = Integer.parseInt(curLine.split(" ")[1]);
-                join(process);
-            } else if(command.equals("find")){
-                int process = Integer.parseInt(curLine.split(" ")[1]);
-                int key = Integer.parseInt(curLine.split(" ")[2]);
-                find(process, key);
-            } else if(command.equals("leave")){
-                int process = Integer.parseInt(curLine.split(" ")[1]);
-                leave(process);
-            } else if(curLine.startsWith("show") && !curLine.equals("show all")){
-                int process = Integer.parseInt(curLine.split(" ")[1]);
-                show(process);
-            }else if(curLine.equals("show all")){
-                showAll();
+                if (command.equals("join")) {
+                    int process = Integer.parseInt(curLine.split(" ")[1]);
+                    join(process);
+                } else if (command.equals("find")) {
+                    int process = Integer.parseInt(curLine.split(" ")[1]);
+                    int key = Integer.parseInt(curLine.split(" ")[2]);
+                    find(process, key);
+                } else if (command.equals("leave")) {
+                    int process = Integer.parseInt(curLine.split(" ")[1]);
+                    leave(process);
+                } else if (curLine.startsWith("show") && !curLine.equals("show all") && !curLine.equals("show count")) {
+                    int process = Integer.parseInt(curLine.split(" ")[1]);
+                    show(process);
+                } else if (curLine.equals("show all")) {
+                    showAll();
+                } else if (curLine.equals("show count")) {
+                    showMessageCount();
+                } else if (curLine.equals("reset count")) {
+                    resetMessageCount();
+                }
             }
+        } else {
+            // Test Stuff
+            int P = 30;
+            int F = 68;
+            Random rand = new Random(System.currentTimeMillis());
+            ArrayList<Integer> nodeList = new ArrayList<Integer>();
+            //nodeList.add(new Integer(0));
+
+            // Add P Nodes
+            for(int i = 0; i< P; i++){
+                Integer k = rand.nextInt(255) + 1;
+                while(nodeList.contains(k)){
+                    k = rand.nextInt(255) + 1;
+                }
+                nodeList.add(k);
+            }
+            for(Integer k : nodeList){
+                join(k);
+            }
+
+            int p1Count = showMessageCount();
+            resetMessageCount();
+
+            for(int i = 0; i < F; i++){
+                int p = rand.nextInt(P);
+                int k = rand.nextInt(255);
+                find(nodeList.get(p),k);
+                try {
+                    Thread.sleep(100);
+                } catch(Exception e){
+
+                }
+            }
+
+            int p2Count = showMessageCount();
+            resetMessageCount();
+
+            print("Test Summary");
+            print("P = " + P);
+            print("F = " + F);
+            print("Phase 1 Total: " + p1Count);
+            print("Phase 2 Total: " + p2Count);
+
         }
         if(outputToFile){
             printWriter.close();
@@ -98,6 +152,7 @@ public class ProcessCoordinator extends Thread {
         }
 
     }
+
 
     public void find(int p, int k){
         if(myThreads[p] != null){
@@ -155,5 +210,25 @@ public class ProcessCoordinator extends Thread {
             }
         }
 
+    }
+
+    public int showMessageCount(){
+        int total = 0;
+        for(int i = 0; i< 256; i++){
+            if(myThreads[i] != null){
+                total += myThreads[i].getNumMessagesSent();
+            }
+        }
+        print("Total # of Messages = " + total);
+        return total;
+    }
+
+    public void resetMessageCount(){
+        for(int i = 0; i < 256; i++){
+            if(myThreads[i] != null){
+                myThreads[i].resetNumMessagesSent();
+            }
+        }
+        print("Reset MessageCounters to 0");
     }
 }
